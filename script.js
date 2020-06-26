24854ce9c27921c1c4bd316d1c3abff73cde697a
Tomas <tomasfreire@gmail.com>
2020-06-26 17:09:44 -0300

    first approach on adding typing with keyboard

--- script.js
+++ script.js
@@ -22,9 +22,35 @@ const screenWriting = (e) => {
   screen.innerHTML += e.target.value;
 };
 
-for(let j=0;j<screenInputs.length;j++){
-  screenInputs[j].addEventListener("click", screenWriting)
+for (let j = 0; j < screenInputs.length; j++) {
+  screenInputs[j].addEventListener("click", screenWriting);
 }
+
+const onKeyPress = (e) => {
+  if (
+    e.key == "0" ||
+    e.key == "1" ||
+    e.key == "2" ||
+    e.key == "3" ||
+    e.key == "4" ||
+    e.key == "5" ||
+    e.key == "6" ||
+    e.key == "7" ||
+    e.key == "8" ||
+    e.key == "9" ||
+    e.key == "+" ||
+    e.key == "-" ||
+    e.key == "*" ||
+    e.key == "/"
+  ) {
+    calculation += e.key;
+    screen.innerHTML += e.key;
+  } else if(e.key == "Enter"){
+    calculate()
+  }
+};
+
+document.addEventListener("keydown", onKeyPress);
 
 const calculate = () => {
   let top = "";
