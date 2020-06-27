0a4ae4c25d20ef4e7f65ab2d089e6873d2f1cc7f
Tomas <tomasfreire@gmail.com>
2020-06-27 19:24:49 -0300

    replaces repeated operators at the end of calculation

--- script.js
+++ script.js
@@ -3,30 +3,50 @@ const topScreen = document.getElementById("top-screen");
 
 let calc = "";
 
+const operators = ["+", "-", "*", "/"];
+
 const inputs = document.getElementsByClassName("input");
-const screenInputs = document.getElementsByClassName("screen-input");
-
-const addCalculation = (e) => {
-  calc += e.target.value;
-};
-
 for (let i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener("click", addCalculation);
 }
 
-const screenWriting = (e) => {
-  topScreen.innerHTML += e.target.value;
-};
-
+const screenInputs = document.getElementsByClassName("screen-input");
 for (let j = 0; j < screenInputs.length; j++) {
   screenInputs[j].addEventListener("click", screenWriting);
 }
 
-const calculate = () => {
-  screen.innerHTML = eval(calc);
+document.addEventListener("keydown", onKeyPress);
+
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substr(0, index) +
+    replacement +
+    this.substr(index + replacement.length)
+  );
 };
 
-const onKeyPress = (e) => {
+function addCalculation(e) {
+  calc += e.target.value;
+  if (
+    operators.includes(calc[calc.length - 2]) &&
+    operators.includes(calc[calc.length - 1])
+  ) {
+    calc = calc.slice(0, -2) + calc.slice(-1);
+    console.log(calc)
+  }
+}
+
+function screenWriting(e) {
+  topScreen.innerHTML = calc;
+}
+
+function calculate() {
+  const result = eval(calc);
+  console.log(result, calc);
+  screen.innerHTML = result;
+}
+
+function onKeyPress(e) {
   if (
     e.key == "0" ||
     e.key == "1" ||
@@ -51,12 +71,10 @@ const onKeyPress = (e) => {
   } else if (e.key == "Backspace") {
     clearAll();
   }
-};
+}
 
-document.addEventListener("keydown", onKeyPress);
-
-const clearAll = () => {
+function clearAll() {
   calc = "";
   screen.innerHTML = "";
   topScreen.innerHTML = "";
-};
+}
